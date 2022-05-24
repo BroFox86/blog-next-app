1944327ab951a7ab11720b71b1f27bf98e65861f
brofox86 <daurgam@gmail.com>
2022-05-24 23:48:46 +0300

    Fix reset of dark theme

--- hooks/useDarkTheme.ts
+++ hooks/useDarkTheme.ts
@@ -7,7 +7,7 @@ import { loadState } from '~/utilities/sessionStorage'
 export function useDarkTheme(app: App) {
   useEffect(() => {
     // Set the last choosed theme
-    if (loadState() && 'darkTheme' in loadState()) {
+    if (loadState()) {
       return handleDarkTheme(app, loadState().darkTheme)
     }
 
@@ -18,6 +18,8 @@ export function useDarkTheme(app: App) {
 
     // Add a listener
     mediaQueryList.addEventListener('change', e => {
+      if (loadState()) return
+
       handleDarkTheme(app, !!e.matches)
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
