197d6165987e0814432c92b959b74c560c8914ec
brofox86 <daurgam@gmail.com>
2022-12-07 16:11:11 +0300

    Use @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '~/styles/global.scss'
 import '~/styles/css-variables.scss'
 import '~/styles/quill.css'
 
+import { Roboto } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Provider } from 'react-redux'
@@ -12,6 +13,28 @@ import { useDarkTheme } from '~/hooks/useDarkTheme'
 import { createMirageServer } from '~/mocks/server'
 import { app } from '~/services/app'
 import store from '~/services/store'
+
+const roboto = Roboto({
+  variable: '--primary-font',
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Oxygen',
+    'Ubuntu',
+    'Cantarell',
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    'Arial',
+    'sans-serif',
+  ],
+})
 
 // if (process.env.NODE_ENV === "development") {
 createMirageServer({ environment: 'development' })
@@ -27,6 +50,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover' />
         <meta name='theme-color' content='#5031aa' />
       </Head>
+      <style jsx global>{`
+        :root {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Provider>
   )
