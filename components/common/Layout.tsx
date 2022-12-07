fba2f4377e3aa7998a01a7a8f883b2ba36f194ea
brofox86 <daurgam@gmail.com>
2022-12-07 16:47:42 +0300

    Fix @next/font

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,3 +1,5 @@
+import { Roboto } from '@next/font/google'
+import clsx from 'clsx'
 import { observer } from 'mobx-react-lite'
 
 import { Header } from '~/components/header/Header'
@@ -5,13 +7,35 @@ import { Header } from '~/components/header/Header'
 import { Footer } from './Footer'
 import s from './Layout.module.scss'
 
+const roboto = Roboto({
+  variable: '--primary-font',
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Oxygen',
+    'Ubuntu',
+    'Cantarell',
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    'Arial',
+    'sans-serif',
+  ],
+})
+
 type Props = {
   children: JSX.Element | JSX.Element[]
 }
 
 export const Layout = observer(({ children }: Props) => {
   return (
-    <div className={s.layout}>
+    <div className={clsx(s.layout, roboto.variable)}>
       <Header />
       <main className={s.main}>{children}</main>
       <Footer />
