2f6873ef9ce5d96719daf3d65bfcd2b3decbbc10
brofox86 <daurgam@gmail.com>
2023-10-20 17:07:14 +0300

    Use conditional props for Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,30 +3,43 @@ import Link from 'next/link'
 
 import s from './Button.module.scss'
 
-type Props = {
-  className?: string
-  variant: string
+type CommonProps = {
+  className?: any
   label: string
-  as?: string
-  type?: 'button' | 'submit' | 'reset' | undefined
-  href?: string
-  isDisabled?: boolean
-  isPending?: boolean
-  onClick?: React.MouseEventHandler
-  children?: JSX.Element
+  variant: 'primary' | 'danger'
+  fullWidth?: boolean
 }
 
-export function Button(props: Props) {
-  const { className, as, label, type, href, isDisabled, isPending, onClick } = props
-  const variant: string = props.variant.toLowerCase()
-  let buttonType
+type LinkProps = {
+  as: 'link'
+  href: string
+  external?: boolean
+  type?: never
+  isDisabled?: never
+  isPending?: never
+  onClick?: never
+}
 
-  if (as !== 'link') {
-    buttonType = type || 'button'
-  }
+type ButtonProps = {
+  onClick: React.MouseEventHandler
+  type?: 'button' | 'submit' | 'reset' | undefined
+  isDisabled?: boolean
+  isPending?: boolean
+  as?: never
+  external?: never
+  href?: never
+}
+
+type ConditionalProps = LinkProps | ButtonProps
+
+type Props = CommonProps & ConditionalProps
+
+export function Button(props: Props) {
+  const { className, as, label, type = 'button', href, isDisabled, isPending, onClick } = props
+  const variant: string = props.variant.toLowerCase()
 
   return as !== 'link' ? (
-    <button className={clsx(s.button, s[variant], className)} type={buttonType} disabled={isDisabled} onClick={onClick}>
+    <button className={clsx(s.button, s[variant], className)} type={type} disabled={isDisabled} onClick={onClick}>
       {label}
       {isPending ? <ButtonSpinner /> : null}
     </button>
