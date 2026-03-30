6b00258de2ea09fa1b20434f40dc719e2d095e74
Daur Gamisonia <daurgam@gmail.com>
2026-03-30 09:17:08 +0300

    Use dompurify for protection

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,11 +1,23 @@
 'use server'
 
+import createDOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { db } from '@/lib/db'
 import { getSluggedText } from '@/utils/format'
 import { wait } from '@/utils/wait'
+
+const window = new JSDOM('').window
+const purify = createDOMPurify(window)
+
+const sanitizeHtml = async (html: string): Promise<string> => {
+  return purify.sanitize(html, {
+    ALLOWED_TAGS: ['h1', 'h2', 'p', 'strong', 'em', 'ul', 'ol', 'li', 'blockquote', 's', 'u'],
+    ALLOWED_ATTR: ['class', 'href', 'target', 'rel', 'data-list']
+  })
+}
 
 export async function getAllPostsAction() {
   wait(500)
@@ -25,12 +37,46 @@ export async function addPostAction(title: string, content: string) {
     data: {
       title: title,
       slug: sluggedTitle,
-      content: content,
+      content: await sanitizeHtml(content),
       imageUrl: '/images/cover-7.jpg'
     }
   })
 
   revalidatePath('/')
+}
+
+export async function getPost(slug: string) {
+  return await db.post.findUnique({
+    where: {
+      slug: slug
+    }
+  })
+}
+
+export async function updatePostAction(id: number, title: string, content: string) {
+  const sluggedTitle = getSluggedText(title)
+
+  await db.post.update({
+    where: { id: id },
+    data: {
+      slug: sluggedTitle,
+      title: title,
+      content: await sanitizeHtml(content)
+    }
+  })
+
+  revalidatePath(`/`)
+  revalidatePath(`/posts/${sluggedTitle}`)
+
+  return sluggedTitle
+}
+
+export async function deletePostAction(id: number) {
+  const deletedPost = await db.post.delete({ where: { id } })
+
+  revalidatePath('/', 'layout')
+
+  return deletedPost.title
 }
 
 export async function handleSearchQuery(formData: FormData) {
@@ -51,38 +97,4 @@ export async function searchPostAction(query: string) {
       OR: [{ title: { contains: query, mode: 'insensitive' } }, { content: { contains: query, mode: 'insensitive' } }]
     }
   })
-}
-
-export async function getPost(slug: string) {
-  return await db.post.findUnique({
-    where: {
-      slug: slug
-    }
-  })
-}
-
-export async function updatePostAction(id: number, title: string, content: string) {
-  const sluggedTitle = getSluggedText(title)
-
-  await db.post.update({
-    where: { id: id },
-    data: {
-      slug: sluggedTitle,
-      title: title,
-      content: content
-    }
-  })
-
-  revalidatePath(`/`)
-  revalidatePath(`/posts/${sluggedTitle}`)
-
-  return sluggedTitle
-}
-
-export async function deletePostAction(id: number) {
-  const deletedPost = await db.post.delete({ where: { id } })
-
-  revalidatePath('/', 'layout')
-
-  return deletedPost.title
 }
